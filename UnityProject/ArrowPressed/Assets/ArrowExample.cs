e2df685776d968e65ad845c5afff9156d174f6e1
Ricardo Pérez <ricardojpf98@gmail.com>
2017-11-07 17:25:08 -0600

    RP
    We already uploaded it our first project to GitHub

---
+++ UnityProject/ArrowPressed/Assets/ArrowExample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowExample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			print ("Left arrow pressed");
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			print ("Right arrow pressed");
+		}
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			print ("Up arrow pressed");
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			print ("Down arrow pressed");
+		}
+	}
+}
